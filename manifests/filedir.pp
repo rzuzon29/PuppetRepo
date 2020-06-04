f3ef0a4a356b7cd3351054d15a52aada0f77f382
Ronald Zuzon <rj.zuzon@yahoo.com>
2020-06-04 16:16:36 -0700

    altaring filedir.pp

--- manifests/filedir.pp
+++ manifests/filedir.pp
@@ -2,7 +2,7 @@ file {'/tmp/filetree/':
 
 	ensure => directory,
 	recurse => true,
-	purge => treu,
+	purge => true,
 	source => '/usr/local/puppet/code/environment/Production/files/config/filetree/',
 	owner => 'rzuzon',
 	mode => '0644',
