c6ce646caecaaa102cddb388d7f04156b75fd5ef
Ronald Zuzon <rj.zuzon@yahoo.com>
2020-06-04 14:25:27 -0700

    changing the dir again

--- manifests/filedir.pp
+++ manifests/filedir.pp
@@ -1,6 +1,7 @@
 file {'/tmp/filedir/':
 
 	ensure => present,
+	ensure => directory,
 	source => '/usr/local/puppet/code/environment/Production/file/config/filedir/',
 	owner => 'rzuzon',
 	mode => '0644',
