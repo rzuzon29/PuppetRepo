98c7d5228f2a8e63b5014124425229bd6df0e760
Ronald Zuzon <rj.zuzon@yahoo.com>
2020-06-04 16:13:01 -0700

    adding purge to the script

--- manifests/filedir.pp
+++ manifests/filedir.pp
@@ -2,6 +2,7 @@ file {'/tmp/filetree/':
 
 	ensure => directory,
 	recurse => true,
+	purge => treu,
 	source => '/usr/local/puppet/code/environment/Production/files/config/filetree/',
 	owner => 'rzuzon',
 	mode => '0644',
